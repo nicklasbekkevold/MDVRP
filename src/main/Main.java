4f8e4c7afa13754597889b17edd92f0ecca0c99d
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-02-14 13:45:25 +0100

    Refactor project structure

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/view/graph.fxml"));
+        primaryStage.setTitle("MDVRP");
+        primaryStage.setScene(new Scene(root, 1024, 576));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
