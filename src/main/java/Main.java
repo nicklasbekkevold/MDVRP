facb25b227de96d2b842c4fa47011fec8f07cd2b
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-02-14 13:54:17 +0100

    Move javafx main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
 package main.java;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/view/graph.fxml"));
+        primaryStage.setTitle("MDVRP");
+        primaryStage.setScene(new Scene(root, 1024, 576));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        launch(args);
     }
 }
