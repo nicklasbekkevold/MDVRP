14a1e55d919466a5ffcb1b471e19eca3982c349e
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-03-15 12:45:06 +0100

    Add banchmark option to plots

--- src/main/scripts/visualize.py
+++ src/main/scripts/visualize.py
@@ -1,8 +1,10 @@
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-training_data_path = "../resources/training_data.txt"
-plot_path = "../resources/training_data.jpg"
+training_data_path = '../resources/training_data.txt'
+plot_path = '../resources/training_data.jpg'
 
 generations, best_durations, average_durations = np.loadtxt(training_data_path, unpack=True)
 
@@ -10,8 +12,18 @@ plt.title('Training data')
 plt.xlabel('generation')
 plt.ylabel('duration')
 
-plt.plot(generations, average_durations, label='Average duration', color='tab:blue')
-plt.plot(generations, best_durations, label='Best duration', color='tab:orange')
+plt.plot(generations, average_durations, label='Average duration', color='k')
+plt.plot(generations, best_durations, label='Best duration', color='deepskyblue')
+
+# Plot benchmarks if benchmark file specified
+if len(sys.argv) > 1:
+    benchmark_path = f'../resources/benchmark_files/{sys.argv[1]}.res'
+    with open(benchmark_path, 'r') as f:
+        best_solution = float(f.readline().strip())
+
+    for percentile in [0.0, 0.05, 0.1, 0.2, 0.3]:
+        generations.fill(best_solution * (1 + percentile))
+        plt.plot(generations, label=str(percentile))
 
 plt.legend()
 plt.savefig(plot_path)
